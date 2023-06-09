15e286a3d77c36db365e19d844d58de6ceb39f83
unknown <nizar.elhilalii@gmail.com>
2023-06-09 07:20:35 +0100

    Last Commit To Deploy

---
+++ src/main/java/com/trung/demo/security/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.trung.demo.security;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Match all paths
+                .allowedOrigins("https://whimsical-peony-04b1a2.netlify.app/") // Add your client URLs here
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Add the HTTP methods allowed by your API
+                .allowedHeaders("*"); // Add the allowed request headers
+    }
+}
